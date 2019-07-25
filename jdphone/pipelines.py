c183420958c823e7688872c2f6f62f6fc8f54c57
jingmiaowei <50897140+jingmiaowei@users.noreply.github.com>
2019-07-24 22:03:33 -0500

    Update pipelines.py

--- jdphone/pipelines.py
+++ jdphone/pipelines.py
@@ -5,19 +5,19 @@ from pymongo import MongoClient
 
 class JdphonePipeline(object):
     def __init__(self):
-        # 获取setting中主机名，端口号和集合名
+        
         host = settings['MONGODB_HOST']
         port = settings['MONGODB_PORT']
         dbname = settings['MONGODB_DBNAME']
         col = settings['MONGODB_COL']
 
-        # 创建一个mongo实例
+        
         client = MongoClient(host=host,port=port)
 
-        # 访问数据库
+       
         db = client[dbname]
 
-        # 访问集合
+        
         self.col = db[col]
 
     def process_item(self, item, spider):
