d309938d6a2326ec27187e8e35c80bafcf999a2c
jingmiaowei <50897140+jingmiaowei@users.noreply.github.com>
2019-07-24 22:02:43 -0500

    Update settings.py

--- jdphone/settings.py
+++ jdphone/settings.py
@@ -11,17 +11,3 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
-
-# 主机环回地址
-MONGODB_HOST = '127.0.0.1'
-# 端口号，默认27017
-MONGODB_POST = 27017
-# 设置数据库名称
-MONGODB_DBNAME = 'JingDong'
-# 设置集合名称
-MONGODB_COL = 'JingDongPhone'
-
-# pipeline设置
-ITEM_PIPELINES = {
-    'jdphone.pipelines.JdphonePipeline': 300,
-}
